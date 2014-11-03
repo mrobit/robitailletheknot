c5d33fc284ca4528d2dd18d67a8a712a208a58e1
Matthew Robitaille <matthew@robitaille.io>
2014-11-02 19:58:19 -0500

    Clean up the url retrieval for tags.

--- app/Knot/Services/InstagramFeedReader.php
+++ app/Knot/Services/InstagramFeedReader.php
@@ -5,21 +5,42 @@ class InstagramFeedReader extends FeedReader {
     /**
      * Gets the feed.
      *
-     * @return mixed
+     * @return array
      */
     public function getFeed()
     {
         parent::getFeed();
 
-        $response = $this->client->get( $this->recentTagMediaUrl() );
+        // Return the cached value if it exists. If not, we'll set it using the Closure
+        // on the "remember" method.
+        $data = $this->cache->remember( $this->tag, 360, function()
+        {
+            $response = $this->client->get( $this->recentTagMediaUrl() );
 
-        $data = $response->json();
+            $data = $response->json();
 
-        return count($data['data']) > 0 ? $data['data'] : [];
+            return $data['data'];
+        });
+
+        return $data;
     }
 
+    /**
+     * Gets the base URL for the API.
+     * @return string
+     */
+    private function getBaseUrl()
+    {
+        return 'https://api.instagram.com/v1/tags/';
+    }
+
+    /**
+     * Gets the url to send to Instagram for the API request.
+     *
+     * @return string
+     */
     public function recentTagMediaUrl()
     {
-        return 'https://api.instagram.com/v1/tags/' . $this->tag . '/media/recent?client_id=' . $this->clientId;
+        return $this->getBaseUrl() .  $this->tag . '/media/recent?client_id=' . $this->clientId;
     }
 }
