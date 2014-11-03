e4ed721e106dfc2f98c5bca355d8ce1d8ef01c43
Matthew Robitaille <matthew@robitaille.io>
2014-11-02 20:11:59 -0500

    Update database config
    Remove  variables since a database is no longer being used.

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $_ENV['DB_HOST'],
-			'database'  => $_ENV['DB_NAME'],
-			'username'  => $_ENV['DB_USER'],
-			'password'  => $_ENV['DB_PASSWORD'],
+			'host'      => 'localhost',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
