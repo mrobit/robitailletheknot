6e6bcb9779eac4baee5288f91a00f1232278f774
Matthew Robitaille <matthew@robitaille.io>
2014-11-06 00:02:32 -0500

    Added Google Analytics tracking code.

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -5,6 +5,8 @@
     <title>Robitaille the Knot</title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/main.min.css"/>
+
+    @include('layouts.partials.analytics')
 </head>
 <body>
 
