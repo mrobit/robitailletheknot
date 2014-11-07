e18edc9f707d5f1639f06cc19298f5d149e43e85
Matthew Robitaille <matthew@robitaille.io>
2014-11-07 14:38:10 -0500

    Meta viewport tag, and head.blade.php partial.

---
+++ app/views/layouts/partials/head.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Robitaille the Knot</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/main.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @include('layouts.partials.analytics')
+</head>
+<body>
+
--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Robitaille the Knot</title>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="/css/main.min.css"/>
-
-    @include('layouts.partials.analytics')
-</head>
-<body>
-
+@include('layouts.partials.head')
 @include('layouts.partials.nav')
 
 <div id="wrap">
