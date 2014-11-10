850ad0f291fedd69707323b77c1430758181207b
Matthew Robitaille <matthew@robitaille.io>
2014-11-09 21:26:02 -0500

    Change fonts from Open Sans to Proxima Nova.

--- app/views/layouts/partials/head.blade.php
+++ app/views/layouts/partials/head.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Robitaille the Knot</title>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/main.min.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//use.typekit.net/mha4sdr.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
 
     @include('layouts.partials.analytics')
 </head>
