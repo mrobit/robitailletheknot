57a0fa25abff717e319a3ad93e6d65fa896b66a3
Matthew Robitaille <matthew@robitaille.io>
2015-02-08 09:59:35 -0500

    Footer styles/content/markup.

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -2,4 +2,8 @@
     <p class="copyright text-center">
         &copy; {{ date("Y") }} robitailletheknot.com | <a href="#top">back to top &uarr;</a>
     </p>
+
+    <p class="text-center">
+        <a href="{{ route('login_path') }}">Log In</a>
+    </p>
 </div>
