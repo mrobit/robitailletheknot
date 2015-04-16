fdc0b2a786ef3fc2929b4da7f34802c3d0960719
Matthew Robitaille <matthew@robitaille.io>
2015-04-15 20:49:40 -0400

    Working on styles, etc.

--- app/views/pages/details.blade.php
+++ app/views/pages/details.blade.php
@@ -1,31 +1,45 @@
 @extends('layouts.application')
 
 @section('content')
-
     <section class="details container">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <h1 class="details__header text-center">
                     Where and when?
                 </h1>
-                <hr/>
-                <p><strong>Where:</strong><br/>
-                    The ceremony will be at Sacred Heart Church in Hallowell, ME.
-                    <br/><br/>
-                    The reception will be held at the Trott Farm.
-                    <br/><br/>
-                    98 Trott Road
-                    <br/>
-                    Richmond, ME
-                    <br/>
-                    04357
-                </p>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="details__column col-sm-6">
+                <h2>
+                    Ceremony
+                </h2>
+
                 <p>
-                    <strong>When:</strong><br/>
-                    The ceremony is at 1:00pm. The reception will start at 3pm.
+                    2:00PM
+                </p>
+
+                <p>
+                    <strong>Sacred Heart Church</strong> <br/>
+                    12 Summer St. <br/>
+                    Hallowell, ME
+                </p>
+            </div>
+            <div class="details__column col-sm-6">
+                <h2>
+                    Reception
+                </h2>
+
+                <p>
+                    4:00PM
+                </p>
+
+                <p>
+                    <strong>Trott Farm</strong> <br/>
+                    98 Trott Rd.<br/>
+                    Richmond, ME
                 </p>
             </div>
         </div>
     </section>
-
 @stop
